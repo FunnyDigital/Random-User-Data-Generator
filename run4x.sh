f80d9bcda6aca78c3709394a47b5bbc1ffb9e66b
Isolokwu Patrick Chukwunonso <46923554+FunnyDigital@users.noreply.github.com>
2025-06-19 15:16:41 +0100

    added a shell script to run fetch data 4 times every 1 min

---
+++ run4x.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+php /home/yourcpanelusername/public_html/fetch_data.php
+sleep 15
+php /home/yourcpanelusername/public_html/fetch_data.php
+sleep 15
+php /home/yourcpanelusername/public_html/fetch_data.php
+sleep 15
+php /home/yourcpanelusername/public_html/fetch_data.php
